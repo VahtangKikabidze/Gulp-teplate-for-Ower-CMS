22ebfead2e7223a8e66bc0c7fc481ab0932847d1
VahtangKikabidze <deryan131089@gmail.com>
2018-08-30 12:25:25 +0300

    Update 30.08.2018

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,9 @@ var gulpif = require('gulp-if');
 var uglify = require('gulp-uglify');
 
 
-
-
-
-
-
 //STYLES
-gulp.task('style', function () {
-  gulp.src('sass/**/*.sass')
+gulp.task('style-main', function () {
+  gulp.src('sass/main.sass')
     .pipe(sass().on('error', sass.logError))
     .pipe(prefix({
     	browsers: ['last 15 versions']
@@ -22,8 +17,31 @@ gulp.task('style', function () {
     .pipe(gulp.dest('templates/site/css'));
 });
 
+gulp.task('style-fonts', function () {
+    gulp.src('sass/fonts.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(prefix({
+            browsers: ['last 15 versions']
+        }))
+        .pipe(gulp.dest('templates/site/css'));
+});
+
+gulp.task('style-media', function () {
+    gulp.src('sass/media.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(prefix({
+            browsers: ['last 15 versions']
+        }))
+        .pipe(gulp.dest('templates/site/css'));
+});
+
+
+
+
 gulp.task('watch', function () {
-  gulp.watch('sass/**/*.sass', ['style']);
+  gulp.watch('sass/main.sass', ['style-main']);
+  gulp.watch('sass/fonts.sass', ['style-fonts']);
+  gulp.watch('sass/media.sass', ['style-media']);
 });
 
 
